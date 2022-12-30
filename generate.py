6f2f59912502bbb701e85d2955b5459b5c7c1c51
Jerome Swannack <jeromeswannack@gmail.com>
2022-12-30 23:36:53 +1300

    Bug fixes and clue updates

--- generate.py
+++ generate.py
@@ -21,12 +21,25 @@ def render_clue(clue: Clue, total: int):
     # Set up the Jinja2 environment
     template_loader = jinja2.FileSystemLoader(searchpath="./jinja_templates/")
     template_env = jinja2.Environment(loader=template_loader)
-
     # Load the template
     template = template_env.get_template("clue.html")
 
     # Render the template with the context
     output = template.render(**clue.dict(), total=total)
+
+    return output
+
+
+def render_clue_index(clues: dict):
+    # Set up the Jinja2 environment
+    template_loader = jinja2.FileSystemLoader(searchpath="./jinja_templates/")
+    template_env = jinja2.Environment(loader=template_loader)
+
+    # Load the template
+    template = template_env.get_template("clue_index.html")
+
+    # Render the template with the context
+    output = template.render(clues=clues)
 
     return output
 
@@ -88,11 +101,8 @@ def main():
         if clue.image_path:
             print(f"Copying {clue.image_path} to site/scavenger_hunt/images")
             output_path = Path("site" + clue.image_uri)
-            output_path.mkdir(parents=True, exist_ok=True)
-            shutil.copy(
-                clue.image_path,
-                str(output_path),
-            )
+            output_path.parent.mkdir(parents=True, exist_ok=True)
+            output_path.write_bytes(Path(clue.image_path).read_bytes())
 
     # render clues
     for name, clue in clues.items():
@@ -101,6 +111,13 @@ def main():
         path.parent.mkdir(parents=True, exist_ok=True)
         print("Saving clue", name, "to", path)
         path.write_text(output)
+
+    # render the clue page and save it
+    output = render_clue_index(sorted(clues.values(), key=lambda clue: clue.number))
+    path = Path("site") / "scavenger_hunt" / "clue_index.html"
+    path.parent.mkdir(parents=True, exist_ok=True)
+    print("Saving clue index to", path)
+    path.write_text(output)
 
 
 if __name__ == "__main__":
